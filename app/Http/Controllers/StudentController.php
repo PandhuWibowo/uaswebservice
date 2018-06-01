25b0882ddca03e103e7224bf42c39f207cf466f6
pandhuwibowo <pandhuw58@gmail.com>
2018-06-01 16:26:31 +0700

    first commit from lumen api

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+// use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\Model;
+// use App\Http\Models\Category;
+use App\Http\Models\Student;
+use Illuminate\Http\Request;
+class StudentController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    //
+    public function index(){
+        try { 
+            $st = Student::all();
+            return response()->json($st, 200);
+        } catch(\Illuminate\Database\QueryException $ex){ 
+            dd($ex->getMessage());
+        }
+    }
+
+    public function detailindex($id){
+        return response()->json(Student::find($id));
+    }
+
+    public function create(Request $request)
+    {
+        $st = Student::create($request->all());
+
+        return response()->json($st, 201);
+    }
+
+    public function update(Request $request,$id)
+    {
+        $st = Student::findOrFail($id);
+        $st->update($request->all());
+        return response()->json($st, 200);
+    }
+
+    public function delete($id)
+    {
+        Student::findOrFail($id)->delete();
+        return response('Deleted Successfully', 200);
+    }
+
+}
---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+$router->get('/students', ['uses' => 'StudentController@index']);
+$router->get('/students/{id}', ['uses' => 'StudentController@detailindex']);
+$router->post('/students', ['uses' => 'StudentController@create']);
+$router->put('/students/{id}', ['uses' => 'StudentController@update']);
+$router->delete('/students/{id}', ['uses' => 'StudentController@delete']);
